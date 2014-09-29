7ca6ca24c5a00fa5d3bf4652887edb2c5c3b3161
emilyhmoore <emily.moore@wustl.edu>
2014-09-29 10:36:26 -0500

    Update Data
    Massively cleans up data

--- datacleanup.R
+++ datacleanup.R
@@ -1,13 +1,9 @@
 setwd("C:/Users/emily m/Documents/GitHub/TYP")
 data<-read.csv("appointments.csv", stringsAsFactors=FALSE)
 
-head(data)
-colnames(data)
-
-library(plyr)
-
 for (i in 1:length(data)){
-  data[i,5]<-paste(data[i,5],data[i,12], sep=" ")
+  
+  data[i,5]<-paste(data[i,5],data[i,12],data[i,13], data[i,14], sep=" ")
 }
 
-head(data[,5])
+head(data[,13],200)
